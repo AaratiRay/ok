9b0d21d348cb0058fa3ca7ba2ae2a929fe2368ac
aaratiray <aaratiray17761@gmail.com>
2024-01-12 14:02:52 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopnavbarComponent } from './topnavbar/topnavbar.component';
+import { SecondnavbarComponent } from './secondnavbar/secondnavbar.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopnavbarComponent,
+    SecondnavbarComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/secondnavbar/secondnavbar.component.ts
@@ -0,0 +1,13 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondnavbar',
+  templateUrl: './secondnavbar.component.html',
+  styleUrls: ['./secondnavbar.component.css']
+})
+export class SecondnavbarComponent implements OnInit {
+  constructor(){}
+  ngOnInit(): void {
+  }
+  
+}
